be987da9d33676da6b906dfd0f39fd211ffa6b01
German Solano <german@famosos.com>
2022-09-08 03:28:57 -0400

    feat: :sparkles: Add Conditional Class to clsx expression only in selection command

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,68 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+function getEditor() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  return editor;
+}
+
+interface ReplaceSelectedTextParams {
+  replaceAll?: boolean;
+  exampleInput: string;
+  validInputChecker: (selectedText: string) => boolean;
+  outputGenerator: (selectedText: string) => string;
+}
+
+function replaceText({
+  replaceAll = false,
+  exampleInput,
+  validInputChecker,
+  outputGenerator
+}: ReplaceSelectedTextParams) {
+  try {
+    const editor = getEditor();
+
+    if (!editor) {
+      throw new Error("No tienes activa ninguna pestaña de edición");
+    }
+
+    const allDocument = new vscode.Range(
+      new vscode.Position(0, 0),
+      new vscode.Position(editor.document.lineCount + 1, 0)
+    );
+    const selection = replaceAll ? allDocument : editor.selection;
+    const selectedText = editor.document.getText(selection);
+
+    if (!validInputChecker(selectedText)) {
+      throw new Error(
+        `El texto seleccionado no concuerda con el tipo de cambio solicitado. Ejm: ${exampleInput}`
+      );
+    }
+
+    editor.edit((edit) => {
+      edit.replace(selection, outputGenerator(selectedText));
+    });
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Ocurrió un error con la extensión: ${error}`
+    );
+  }
+}
+
+const conditionalClassRegExp = /\[class\.(.*?)\]="(.*?)"/gi;
+
+const conditionalClassToClsxExpressionParams: ReplaceSelectedTextParams = {
+  exampleInput: `[class.someClass]="someBoolean"`,
+  validInputChecker: (selectedText: string) =>
+    conditionalClassRegExp.test(selectedText),
+  outputGenerator: (selectedText: string) =>
+    selectedText.replace(conditionalClassRegExp, `$2 && "$1"`)
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -12,29 +74,15 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "reactngular.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showWarningMessage("Hello VS Code");
-    }
-  );
+  let selectionConditionalClassToClsxExpression =
+    vscode.commands.registerCommand(
+      "reactngular.selectionConditionalClassToClsxExpression",
+      () => {
+        replaceText(conditionalClassToClsxExpressionParams);
+      }
+    );
 
-  context.subscriptions.push(disposable);
-
-  let disposable2 = vscode.commands.registerCommand(
-    "reactngular.displayCurrentTime",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showWarningMessage(
-        "Hello VS Code\n" + new Date().toLocaleDateString("es-ES")
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable2);
+  context.subscriptions.push(selectionConditionalClassToClsxExpression);
 }
 
 // this method is called when your extension is deactivated
